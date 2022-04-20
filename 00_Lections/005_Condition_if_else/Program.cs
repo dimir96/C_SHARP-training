5d4c99eca66f39843f5126d9287b417ab60663f6
Dmytro Ryabov <dimir996@gmail.com>
2022-04-20 22:26:40 +0200

    005 Condition if else

---
+++ 00_Lections/005_Condition_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Vvieditie inmja polzowatela: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("URA eto ze MASHA!!!");
+}
+else
+{
+    Console.Write("Privet, ");
+    Console.WriteLine(username);
+}
