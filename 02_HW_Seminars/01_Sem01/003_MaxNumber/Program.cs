6a32c37fe5942c9ec555d211e3fe1ce87650c8e8
Ryabov Dmitry <dimir996@gmail.com>
2022-05-25 22:23:54 +0200

    corrected HW

--- 02_HW_Seminars/01_Sem01/003_MaxNumber/Program.cs
+++ 02_HW_Seminars/01_Sem01/003_MaxNumber/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.Write("Введите первое число: ");
+﻿// Задача 2: Напишите программу, которая принимает на
+// вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите первое число: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
