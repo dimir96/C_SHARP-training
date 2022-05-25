ac769ddabdeeeb0ef7d5ae109df0b260ff605d8e
Ryabov Dmitry <dimir996@gmail.com>
2022-05-26 00:09:30 +0200

    sem 002

---
+++ 00_Lections/009_IntroArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2>result) result=arg2;
+    if (arg3>result) result=arg3;
+    return result;
+}
+
+
+int[] array = {41,52,38,44,88,756,75,87,98};
+
+
+Console.WriteLine(array[0]);
---
+++ 00_Lections/010_MethodFind/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {17,27,473,4,5,476,774,48,9};
+
+int n = array.Length;
+int find = 476;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    
+    index++;
+}
